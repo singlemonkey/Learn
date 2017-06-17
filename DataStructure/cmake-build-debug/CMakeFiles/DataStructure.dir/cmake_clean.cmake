e18f72438fdcb791f4fb5e71616a69d20bd2f23a
Sadness <903012206@qq.com>
2017-06-18 01:17:06 +0800

    Add repos

---
+++ DataStructure/cmake-build-debug/CMakeFiles/DataStructure.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructure.dir/main.cpp.o"
+  "DataStructure.pdb"
+  "DataStructure"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataStructure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
